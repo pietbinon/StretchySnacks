be982d389d9d133751ae027ee5fac33cccca617f
Graeme Harrison <gharrison365@gmail.com>
2016-02-18 12:09:26 -0500

    added fake nav bar that changes size if button is pressed

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var navBarView: UIView!
+    @IBOutlet var navBarHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func plusButton(sender: UIButton) {
+        if self.navBarHeightConstraint.constant == 64 {
+            self.navBarHeightConstraint.constant = 200
+            UIView.animateWithDuration(0.5) {
+                self.view.layoutIfNeeded()
+            }
+        }
+        else {
+            self.navBarHeightConstraint.constant = 64
+            UIView.animateWithDuration(0.5) {
+                self.view.layoutIfNeeded()
+            }
+        }
+        
     }
-
-
 }
 
