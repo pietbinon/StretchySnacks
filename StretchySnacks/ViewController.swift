998543686ef72da6d837ad8a08a3b0d102562f8a
Graeme Harrison <gharrison365@gmail.com>
2016-02-18 12:36:30 -0500

    Nav bar plus button rotates with spring animation when pressed

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -10,28 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var plusButton: UIButton!
     @IBOutlet var navBarView: UIView!
     @IBOutlet var navBarHeightConstraint: NSLayoutConstraint!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func plusButton(sender: UIButton) {
+        navBarSpringAnimation()
+        plusButtonRotateAnimation()
+    }
+    
+    func plusButtonRotateAnimation() {
+        UIView.animateWithDuration(0.8,
+            delay: 0.0,
+            usingSpringWithDamping: 0.3,
+            initialSpringVelocity: 0.1,
+            options: .CurveLinear,
+            animations: {self.plusButton.transform = CGAffineTransformRotate(self.plusButton.transform, CGFloat(M_PI_2))},
+            completion: {(value: Bool) in })
+    }
+    
+    func navBarSpringAnimation() {
         if self.navBarHeightConstraint.constant == 64 {
             self.navBarHeightConstraint.constant = 200
-            UIView.animateWithDuration(0.5) {
+            
+            //            UIView.animateWithDuration(0.5) {
+            //                self.view.layoutIfNeeded()
+            //            }
+            
+            UIView.animateWithDuration(1.0,
+                delay: 0.0,
+                usingSpringWithDamping: 0.3,
+                initialSpringVelocity: 0.8,
+                options: .CurveLinear,
+                animations: {
                 self.view.layoutIfNeeded()
-            }
+                }, completion: {
+                    (value: Bool) in
+            })
         }
         else {
             self.navBarHeightConstraint.constant = 64
-            UIView.animateWithDuration(0.5) {
+            UIView.animateWithDuration(1.0,
+                delay: 0.0,
+                usingSpringWithDamping: 0.3,
+                initialSpringVelocity: 0.8,
+                options: .CurveLinear,
+                animations: {
                 self.view.layoutIfNeeded()
-            }
+                }, completion: {
+                    (value: Bool) in
+            })
         }
-        
     }
 }
 
