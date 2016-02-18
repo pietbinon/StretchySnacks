eb2777663e15f4d16fe7f78b4da7e35905dae5ad
Graeme Harrison <gharrison365@gmail.com>
2016-02-18 14:41:50 -0500

    Added tableview. Snacks  are added to array and displayed in tableview cells when pics are pressed

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var plusButton: UIButton!
     @IBOutlet var navBarView: UIView!
     @IBOutlet var navBarHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var tableView: UITableView!
     
     var stackView : UIStackView = UIStackView()
+    var snacksArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,30 +26,46 @@ class ViewController: UIViewController {
     
     func prepareNavBar() {
         
-        let oreosView : UIImageView = UIImageView()
-        oreosView.heightAnchor.constraintEqualToConstant(120.0).active = true
-        oreosView.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
-        oreosView.image = UIImage(named: "oreos")
+//        let oreosView : UIImageView = UIImageView()
+//        oreosView.heightAnchor.constraintEqualToConstant(120.0).active = true
+//        oreosView.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
+//        oreosView.image = UIImage(named: "oreos")
+//        oreoButton.addSubview(oreosView)
 
-        let pizzaPocketsView : UIImageView = UIImageView()
-        pizzaPocketsView.heightAnchor.constraintEqualToConstant(120.0).active = true
-        pizzaPocketsView.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
-        pizzaPocketsView.image = UIImage(named: "pizza_pockets")
+        let oreoButton : UIButton = UIButton()
+        oreoButton.heightAnchor.constraintEqualToConstant(120.0).active = true
+        oreoButton.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
+        oreoButton.tag = 1
+        oreoButton.addTarget(self, action: "addSnack:", forControlEvents: .TouchUpInside)
+        oreoButton.setImage(UIImage(named: "oreos"), forState: .Normal)
         
-        let popTartsView : UIImageView = UIImageView()
-        popTartsView.heightAnchor.constraintEqualToConstant(120.0).active = true
-        popTartsView.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
-        popTartsView.image = UIImage(named: "pop_tarts")
+        let pizzaPocketsButton = UIButton()
+        pizzaPocketsButton.heightAnchor.constraintEqualToConstant(120.0).active = true
+        pizzaPocketsButton.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
+        pizzaPocketsButton.tag = 2
+        pizzaPocketsButton.addTarget(self, action: "addSnack:", forControlEvents: .TouchUpInside)
+        pizzaPocketsButton.setImage(UIImage(named: "pizza_pockets"), forState: .Normal)
         
-        let popsicleView : UIImageView = UIImageView()
-        popsicleView.heightAnchor.constraintEqualToConstant(120.0).active = true
-        popsicleView.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
-        popsicleView.image = UIImage(named: "popsicle")
+        let popTartsButton = UIButton()
+        popTartsButton.heightAnchor.constraintEqualToConstant(120.0).active = true
+        popTartsButton.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
+        popTartsButton.tag = 3
+        popTartsButton.addTarget(self, action: "addSnack:", forControlEvents: .TouchUpInside)
+        popTartsButton.setImage(UIImage(named: "pop_tarts"), forState: .Normal)
         
-        let ramenView : UIImageView = UIImageView()
-        ramenView.heightAnchor.constraintEqualToConstant(120.0).active = true
-        ramenView.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
-        ramenView.image = UIImage(named: "ramen")
+        let popsicleButton = UIButton()
+        popsicleButton.heightAnchor.constraintEqualToConstant(120.0).active = true
+        popsicleButton.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
+        popsicleButton.tag = 4
+        popsicleButton.addTarget(self, action: "addSnack:", forControlEvents: .TouchUpInside)
+        popsicleButton.setImage(UIImage(named: "popsicle"), forState: .Normal)
+        
+        let ramenButton = UIButton()
+        ramenButton.heightAnchor.constraintEqualToConstant(120.0).active = true
+        ramenButton.widthAnchor.constraintEqualToConstant(self.view.frame.size.width/5.0).active = true
+        ramenButton.tag = 5
+        ramenButton.addTarget(self, action: "addSnack:", forControlEvents: .TouchUpInside)
+        ramenButton.setImage(UIImage(named: "ramen"), forState: .Normal)
         
         //Stack View
         stackView.axis = UILayoutConstraintAxis.Horizontal
@@ -56,11 +74,11 @@ class ViewController: UIViewController {
         stackView.hidden = true
         //        stackView.spacing = 1.0
         
-        stackView.addArrangedSubview(oreosView)
-        stackView.addArrangedSubview(pizzaPocketsView)
-        stackView.addArrangedSubview(popTartsView)
-        stackView.addArrangedSubview(popsicleView)
-        stackView.addArrangedSubview(ramenView)
+        stackView.addArrangedSubview(oreoButton)
+        stackView.addArrangedSubview(pizzaPocketsButton)
+        stackView.addArrangedSubview(popTartsButton)
+        stackView.addArrangedSubview(popsicleButton)
+        stackView.addArrangedSubview(ramenButton)
         stackView.translatesAutoresizingMaskIntoConstraints = false;
         
         navBarView.addSubview(stackView)
@@ -68,6 +86,34 @@ class ViewController: UIViewController {
         //Constraints
         stackView.centerXAnchor.constraintEqualToAnchor(self.navBarView.centerXAnchor).active = true
         stackView.bottomAnchor.constraintEqualToAnchor(self.navBarView.bottomAnchor).active = true
+    }
+    
+    func addSnack(sender: UIButton) {
+        switch (sender.tag) {
+        case 1:
+            let oreo = "Oreo"
+            snacksArray.append(oreo)
+        
+        case 2:
+            let pizzaPocket = "Pizza Pocket"
+            snacksArray.append(pizzaPocket)
+            
+        case 3:
+            let popTart = "Pop Tart"
+            snacksArray.append(popTart)
+            
+        case 4:
+            let popsicle = "Popsicle"
+            snacksArray.append(popsicle)
+            
+        case 5:
+            let ramen = "Ramen"
+            snacksArray.append(ramen)
+        
+        default:
+            print("something ain't right!")
+        }
+        tableView.reloadData()
     }
     
     @IBAction func plusButton(sender: UIButton) {
@@ -119,6 +165,18 @@ class ViewController: UIViewController {
                     (value: Bool) in
             })
         }
+    }
+    
+    //MARK: Table View
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snacksArray.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        let snack = snacksArray[indexPath.row] as String
+        cell.textLabel!.text = snack
+        return cell
     }
 }
 
